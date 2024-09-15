92e3ce47de11ef2da39e98aa428b95fa21c968ae
maliciousbucket <85473030+maliciousbucket@users.noreply.github.com>
2024-09-15 17:24:04 +1000

    fix traefik permissions and ports for cddk8s synth

--- pkg/kubernetes/ingress.go
+++ pkg/kubernetes/ingress.go
@@ -5,11 +5,13 @@ import (
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
 	kplus "github.com/cdk8s-team/cdk8s-plus-go/cdk8splus30/v2"
+	"github.com/maliciousbucket/plumage/imports/traefikio"
 )
 
 const (
-	defaultIngressName  = "traefik-ingress-controller"
-	defaultTraefikImage = "traefik:v3.1"
+	defaultIngressName   = "traefik-ingress-controller"
+	defaultTraefikImage  = "traefik:v3.1"
+	entrypointAnnotation = "traefik.ingress.kubernetes.io/router-entrypoints"
 )
 
 func defaultIngressLabels() *map[string]*string {
@@ -140,34 +142,198 @@ func DefaultIngressDeployment(scope constructs.Construct, id string, namespace s
 	return kplus.NewDeployment(scope, jsii.String(id), props)
 }
 
-//func OtherIngress(scope constructs.Construct, namespace string) k8s.KubeService {
-//	label := map[string]*string{"app": jsii.String("traefik")}
-//
-//	return k8s.NewKubeService(scope, jsii.String("better-service"), &k8s.KubeServiceProps{
-//		Metadata: &k8s.ObjectMeta{
-//			Name:      jsii.String(defaultIngressName),
-//			Namespace: jsii.String(namespace),
-//		},
-//		Spec: &k8s.ServiceSpec{
-//			LoadBalancerClass:        nil,
-//			LoadBalancerIp:           nil,
-//			LoadBalancerSourceRanges: nil,
-//			Ports: &[]*k8s.ServicePort{
-//				&k8s.ServicePort{
-//					Name:       jsii.String("web"),
-//					Protocol:   jsii.String("TCP"),
-//					Port:       jsii.Number(8080),
-//					TargetPort: k8s.IntOrString_FromNumber(jsii.Number(8080)),
-//				},
-//				&k8s.ServicePort{
-//					Name:     jsii.String("admin"),
-//					Protocol: jsii.String("TCP"),
-//
-//					Port:       jsii.Number(8000),
-//					TargetPort: k8s.IntOrString_FromNumber(jsii.Number(8000)),
-//				},
-//			},
-//			Selector: &label,
-//		},
-//	})
-//}
+func NewTraefikIngress(scope constructs.Construct, id string, ns string) constructs.Construct {
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cdk8s.ChartProps{
+		DisableResourceNameHashes: jsii.Bool(true),
+		Namespace:                 jsii.String(ns),
+	})
+
+	roleImp := cdk8s.NewInclude(chart, jsii.String("traefik-import-role"), &cdk8s.IncludeProps{
+		Url: jsii.String("dist/include/traefik-cluster-role.yaml"),
+	})
+
+	roleObj := *roleImp.ApiObjects()
+
+	acc := kplus.NewServiceAccount(chart, jsii.String("yo,"), &kplus.ServiceAccountProps{
+		Metadata: &cdk8s.ApiObjectMetadata{
+			Name:      jsii.String("traefik-ingress-controller"),
+			Namespace: jsii.String(ns),
+		},
+	})
+
+	role := kplus.ClusterRole_FromClusterRoleName(chart, jsii.String("traefik-cluster-role"), roleObj[0].Name())
+	binding := kplus.NewClusterRoleBinding(chart, jsii.String("traefik-role-binding"), &kplus.ClusterRoleBindingProps{
+		Metadata: &cdk8s.ApiObjectMetadata{
+			Annotations:     nil,
+			Finalizers:      nil,
+			Labels:          nil,
+			Name:            jsii.String("traefik-role-binding"),
+			Namespace:       nil,
+			OwnerReferences: nil,
+		},
+		Role: role,
+	})
+
+	binding.AddSubjects(acc)
+
+	args := traefikIngressArgs()
+	labels := traefikIngressLabels()
+	annotations := traefikIngressAnnotations(ns)
+
+	deployment := kplus.NewDeployment(chart, jsii.String(id), &kplus.DeploymentProps{
+		Metadata: &cdk8s.ApiObjectMetadata{
+			Namespace:   jsii.String(ns),
+			Name:        jsii.String("traefik"),
+			Labels:      &labels,
+			Annotations: &annotations,
+		},
+		AutomountServiceAccountToken: jsii.Bool(true),
+		Containers:                   nil,
+		Dns:                          nil,
+		DockerRegistryAuth:           nil,
+		HostAliases:                  nil,
+		HostNetwork:                  nil,
+		InitContainers:               nil,
+		Isolate:                      nil,
+		RestartPolicy:                "",
+		SecurityContext: &kplus.PodSecurityContextProps{
+			EnsureNonRoot:       jsii.Bool(false),
+			FsGroup:             nil,
+			FsGroupChangePolicy: "",
+			Group:               nil,
+			Sysctls:             nil,
+			User:                nil,
+		},
+		ServiceAccount:         acc,
+		TerminationGracePeriod: nil,
+		Volumes:                nil,
+		PodMetadata: &cdk8s.ApiObjectMetadata{
+			Annotations:     nil,
+			Finalizers:      nil,
+			Labels:          &labels,
+			Name:            nil,
+			Namespace:       nil,
+			OwnerReferences: nil,
+		},
+		Select:           nil,
+		Spread:           nil,
+		MinReady:         nil,
+		ProgressDeadline: nil,
+		Replicas:         jsii.Number(1),
+		Strategy:         nil,
+	})
+	deployment.AddContainer(&kplus.ContainerProps{
+		Args:            &args,
+		EnvFrom:         nil,
+		EnvVariables:    nil,
+		ImagePullPolicy: kplus.ImagePullPolicy_IF_NOT_PRESENT,
+		Name:            jsii.String("traefik"),
+		Ports: &[]*kplus.ContainerPort{
+			{
+
+				Name:   jsii.String("web"),
+				Number: jsii.Number(80),
+				//HostPort: jsii.Number(80),
+			},
+			{
+
+				Name:   jsii.String("admin"),
+				Number: jsii.Number(8080),
+				//HostPort: jsii.Number(8080),
+			},
+			{
+
+				Name:   jsii.String("websecure"),
+				Number: jsii.Number(4443),
+				//HostPort: jsii.Number(4443),
+			},
+		},
+		SecurityContext: &kplus.ContainerSecurityContextProps{
+			AllowPrivilegeEscalation: nil,
+			Capabilities:             nil,
+			EnsureNonRoot:            jsii.Bool(false),
+			Group:                    nil,
+			Privileged:               nil,
+			ReadOnlyRootFilesystem:   nil,
+			SeccompProfile:           nil,
+			User:                     nil,
+		},
+		Startup:      nil,
+		VolumeMounts: nil,
+		WorkingDir:   nil,
+		Image:        jsii.String(defaultTraefikImage),
+	})
+	selector := kplus.LabelSelector_Of(&kplus.LabelSelectorOptions{
+		Expressions: nil,
+		Labels:      &labels,
+	})
+	deployment.Select(selector)
+
+	web := deployment.ExposeViaService(&kplus.DeploymentExposeViaServiceOptions{
+		Name: jsii.String("traefik-web-service"),
+		Ports: &[]*kplus.ServicePort{
+			{
+				Protocol:   kplus.Protocol_TCP,
+				TargetPort: jsii.Number(80),
+				Port:       jsii.Number(80),
+			},
+		},
+		ServiceType: kplus.ServiceType_LOAD_BALANCER,
+	})
+
+	web.SelectLabel(jsii.String("app"), jsii.String("traefik"))
+
+	db := deployment.ExposeViaService(&kplus.DeploymentExposeViaServiceOptions{
+		Name: jsii.String("traefik-dashboard-service"),
+		Ports: &[]*kplus.ServicePort{
+			{
+				Protocol:   kplus.Protocol_TCP,
+				TargetPort: jsii.Number(8080),
+				Port:       jsii.Number(8080),
+			},
+		},
+		ServiceType: kplus.ServiceType_LOAD_BALANCER,
+	})
+
+	db.SelectLabel(jsii.String("app"), jsii.String("traefik"))
+
+	return chart
+
+}
+
+func traefikIngressAnnotations(ns string) map[string]*string {
+	annotations := map[string]*string{
+		//entrypointAnnotation: jsii.String("web"),
+	}
+	return annotations
+}
+
+func traefikIngressLabels() map[string]*string {
+	labels := map[string]*string{
+		"app": jsii.String("traefik"),
+	}
+	return labels
+}
+
+func traefikIngressArgs() []*string {
+	args := []*string{
+		jsii.String("--api.insecure"),
+		jsii.String("--accesslog"),
+		jsii.String("--entryPoints.web.Address=:80"),
+		jsii.String("--providers.kubernetescrd"),
+		jsii.String("--entryPoints.websecure.Address=:4443"),
+		jsii.String("--entryPoints.traefik.Address=:8080"),
+		//jsii.String("allowEmptyServices: true"),
+	}
+	return args
+}
+
+func test(scope constructs.Construct, id string) {
+	traefikio.NewTraefikService(scope, jsii.String(id), &traefikio.TraefikServiceProps{
+		Metadata: nil,
+		Spec: &traefikio.TraefikServiceSpec{
+			Mirroring: nil,
+			Weighted:  nil,
+		},
+	})
+}

0637278a92ba30e81d1cb77ef3de5b0f94db1f72
maliciousbucket <85473030+maliciousbucket@users.noreply.github.com>
2024-10-09 20:29:12 +1100

    feat: scaffold integration tests

--- pkg/integration-testing/helm_charts_test.go
+++ pkg/integration-testing/helm_charts_test.go
@@ -1,1 +1,78 @@
 package integration_testing
+
+import (
+	"context"
+	"github.com/maliciousbucket/plumage/internal/helm"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+var (
+	argoVersion             = "argo-cd-7.6.1"
+	promOperatorVersion     = "14.0.0"
+	kubeStateMetricsVersion = "5.25.1"
+	certManagerVersion      = "v1.15.3"
+	k6OperatorVersion       = "3.9.0"
+)
+
+type HelmClient interface {
+	InstallArgoChart(ctx context.Context, version, valuesFile string) error
+	InstallKubeMetricsServerChart(ctx context.Context, version string, replace bool) error
+	InstallCertManagerChart(ctx context.Context, version string, replace bool) error
+	InstallPromOperatorCRDs(ctx context.Context, version string, replace bool) error
+	InstallBaseCharts(ctx context.Context, opts *helm.BaseChartOpts, replace bool) error
+	InstallK6(ctx context.Context, version string, replace bool) error
+	InstallChart(ctx context.Context, chart *helm.ChartConfig) error
+}
+
+func newHelmClient(kubeConfig []byte) (HelmClient, error) {
+	cfg := &helm.ClientCfg{}
+	return helm.NewClientFromConfig(cfg, kubeConfig)
+}
+
+func TestInstallBaseCharts(t *testing.T) {
+
+	kubeContainer := NewKubernetesContainer()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+	err := SetupKubernetesContainer(ctx, kubeContainer)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err = kubeContainer.Container.Terminate(ctx); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	config, err := kubeContainer.KubeConfig(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client, err := newHelmClient(config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Install ArgoCD Chart", func(t *testing.T) {
+		err = client.InstallArgoChart(ctx, argoVersion, "")
+		assert.NoError(t, err)
+	})
+
+	t.Run("Install Prometheus Operator CRDs", func(t *testing.T) {
+		err = client.InstallPromOperatorCRDs(ctx, promOperatorVersion, false)
+		assert.NoError(t, err)
+	})
+
+	t.Run("Install K6 Operator", func(t *testing.T) {
+		err = client.InstallK6(ctx, k6OperatorVersion, false)
+	})
+
+	t.Run("Install CertManager Chart", func(t *testing.T) {
+		err = client.InstallCertManagerChart(ctx, certManagerVersion, false)
+		assert.NoError(t, err)
+	})
+
+}
--- pkg/integration-testing/k3s.go
+++ pkg/integration-testing/k3s.go
@@ -1,1 +1,109 @@
 package integration_testing
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/k3s"
+	"github.com/testcontainers/testcontainers-go/wait"
+	"time"
+)
+
+var (
+	k3sImage = "docker.io/rancher/k3s:v1.27.1-k3s1"
+)
+
+type KubernetesContainer struct {
+	Container *k3s.K3sContainer
+	Options   []testcontainers.ContainerCustomizer
+}
+
+func NewKubernetesContainer() *KubernetesContainer {
+	return &KubernetesContainer{Options: []testcontainers.ContainerCustomizer{}}
+}
+
+func SetupKubernetesContainer(ctx context.Context, c *KubernetesContainer) error {
+
+	k3sContainer, err := k3s.Run(ctx, "docker.io/rancher/k3s:v1.27.1-k3s1", c.Options...)
+	if err != nil {
+		return fmt.Errorf("error creating k3s container: %w", err)
+	}
+
+	if k3sContainer != nil {
+		c.Container = k3sContainer
+	} else {
+		return fmt.Errorf("k3s container not found")
+	}
+
+	return nil
+
+}
+
+func (c *KubernetesContainer) KubeConfig(ctx context.Context) ([]byte, error) {
+	if c.Container == nil {
+		return nil, fmt.Errorf("k3s container not found")
+	}
+	config, err := c.Container.GetKubeConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+type KubeContainerOption func(c *KubernetesContainer) error
+
+func WithManifests(manifests []string) KubeContainerOption {
+	return func(c *KubernetesContainer) error {
+		if manifests == nil {
+			return fmt.Errorf("manifests is nil")
+		}
+		for _, manifest := range manifests {
+			c.Options = append(c.Options, k3s.WithManifest(manifest))
+		}
+		return nil
+	}
+}
+
+type WaitCondition struct {
+	Resource  string
+	Name      string
+	Namespace string
+	Condition string
+	Timeout   time.Duration
+}
+
+func (w *WaitCondition) Validate() error {
+	var err error
+	if w.Resource == "" {
+		err = errors.Join(err, errors.New("resource is empty"))
+	}
+	if w.Name == "" {
+		err = errors.Join(err, errors.New("name is empty"))
+	}
+	if w.Condition == "" {
+		err = errors.Join(err, errors.New("condition is empty"))
+	}
+	if w.Timeout == 0 {
+		err = errors.Join(err, errors.New("timeout is empty"))
+	}
+	return err
+}
+
+func WithWaitStrategy(condition *WaitCondition) KubeContainerOption {
+	return func(c *KubernetesContainer) error {
+		if err := condition.Validate(); err != nil {
+			return err
+		}
+		strategy := []string{"kubectl", "wait"}
+		strategy = append(strategy, condition.Resource, condition.Name)
+		if condition.Namespace != "" {
+			namespace := fmt.Sprintf("--namespace=%s", condition.Namespace)
+			strategy = append(strategy, namespace)
+		}
+		timeout := fmt.Sprintf("--timeout=%s", condition.Timeout)
+		strategy = append(strategy, timeout)
+		c.Options = append(c.Options, testcontainers.WithWaitStrategy(wait.ForExec(strategy)))
+		return nil
+	}
+}
